cb01c14379931ab60e43eb92419da5eac36ec96c
Saulo Teixeira <sauloadolfo32@gmail.com>
2024-09-30 19:04:03 -0300

    config 2

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -2,13 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Posts } from "./posts";
 import { Post } from "./post";
 
-
 const AppRoutes = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route exact path="rotas/" element={<Posts />} />
-				<Route exact path="rotas/post/:id" element={<Post />} />
+				<Route exact path="/" element={<Posts />} />
+				<Route exact path="/post/:id" element={<Post />} />
 			</Routes>
 		</BrowserRouter>
 	);
